77ae12bb7caf2e290c68f51027e717d41a7eb636
ElizabethSobiya <ellizabethsobiya@gmail.com>
2023-10-31 20:11:15 +0530

    New experience and form changes are added

--- script.js
+++ script.js
@@ -34,3 +34,26 @@ document.querySelectorAll('a').forEach(links =>{
     }
 
 });
+
+  const form = document.getElementById('contact-form');
+  form.addEventListener('submit', async (e) => {
+    e.preventDefault();
+    const formData = new FormData(form);
+
+    const response = await fetch('https://formspree.io/f/mpzgnakp', {
+      method: 'POST',
+      body: formData,
+      headers: {
+        Accept: 'application/json',
+      },
+    });
+
+    if (response.ok) {
+      // Successful submission
+      alert('Message sent successfully!');
+      form.reset();
+    } else {
+      // Error handling
+      alert('Oops! Something went wrong. Please try again later.');
+    }
+  });
